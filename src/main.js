822a4b8eba1aae4898bd7bb68c5151d0b1fb4fde
Moaid <moaidnasser19@gmail.com>
2025-08-12 13:52:36 -0700

    Define state and DOM.

--- src/main.js
+++ src/main.js
@@ -3,3 +3,12 @@ const FAV_KEY = 'catalog:favorites';
 const listURL = (skip, limit) => `https://cataas.com/api/cats?skip=${skip}&limit=${limit}`;
 const imgURL = (id) => `https://cataas.com/cat/${id}`;
 
+let page = 1;
+
+const view = document.getElementById('view');
+const controls = document.getElementById('controls');
+const pageNo = document.getElementById('pageNo');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+const navHome = document.getElementById('nav-home');
+const navFav = document.getElementById('nav-fav');
